67879021ec64944f121b6a4d80f9e961e13d30b8
holden_grant <holdengrantcausing@gmail.com>
2018-05-24 16:03:14 +0800

    new design

--- js/my-js.js
+++ js/my-js.js
@@ -3,10 +3,21 @@ $(function(){
    	// alert("test");
    	$(this).addClass('move-left');
    	$('#my-work').addClass('show-canvas');
+    $('#home-menu').addClass('work-menu-right');
    });
+
+   $("#home-menu").on('click', function(){
+    // alert("test");
+    $('#work-menu').removeClass('move-left');
+    $('#my-work').removeClass('show-canvas');
+    $(this).removeClass('work-menu-right');
+   });
+
+
    if(window.location.href.indexOf("#work") > -1) {
        //alert("your url contains the name franky");
        $('#work-menu').addClass('move-left');
        $('#my-work').addClass('show-canvas');
+       $('#home-menu').addClass('work-menu-right');
     }
 });
