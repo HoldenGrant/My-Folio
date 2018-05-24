f97f6cceb45a24c4d91ce72f1bc8466b4bddc99e
HoldenGrant <holdengrantcausing@gmail.com>
2018-05-24 21:12:05 +0800

    complete side design

--- js/my-js.js
+++ js/my-js.js
@@ -20,4 +20,15 @@ $(function(){
        $('#my-work').addClass('show-canvas');
        $('#home-menu').addClass('work-menu-right');
     }
+
+    $('#home-menu').hover(
+        function(e){
+          //alert("test1");
+          $('#my-work .side-black .side-border').css({'width':'20px','transition': '.5s'});
+        }, // over
+        function(e){
+          //alert("test2");
+          $('#my-work .side-black .side-border').css({'width':'0px','transition': '.5s'});
+        }  // out
+    );
 });
