4039eca0949e770e589fb5c9c01bc88085857b87
holdengrant <holdengrantcausing@gmail.com>
2018-05-25 10:17:09 +0800

    fadeinup effect

--- js/my-js.js
+++ js/my-js.js
@@ -1,36 +1,55 @@
 jQuery(function($){
-   $("#work-menu").on('click', function(){
-   	// alert("test");
-   	$(this).addClass('move-left');
-   	$('#my-work').addClass('show-canvas');
-    $('#home-menu').addClass('work-menu-right');
-   });
+	$('#gallery-images-holder .card').css('opacity', 0);
+	$("#work-menu").on('click', function(){
+		$(this).addClass('move-left');
+		$('#my-work').addClass('show-canvas');
+		$('#home-menu').addClass('work-menu-right');
+		setTimeout(
+			function() {
+			$('#gallery-images-holder .card').waypoint(function() {
+				$('#gallery-images-holder .card').addClass('fadeInUp');
+			}, { offset: '500%' });
+		}, 1200);
+	});
 
-   $("#home-menu").on('click', function(){
-    // alert("test");
-    $('#work-menu').removeClass('move-left');
-    $('#my-work').removeClass('show-canvas');
-    $(this).removeClass('work-menu-right');
-   });
-
+	$("#home-menu").on('click', function(){
+		$('#work-menu').removeClass('move-left');
+		$('#my-work').removeClass('show-canvas');
+		$(this).removeClass('work-menu-right');
+		$('#gallery-images-holder .card').css('opacity', 0);
+		$('#gallery-images-holder .card').removeClass('fadeInUp');
+	});
 
    if(window.location.href.indexOf("#work") > -1) {
-       //alert("your url contains the name franky");
        $('#work-menu').addClass('move-left');
        $('#my-work').addClass('show-canvas');
        $('#home-menu').addClass('work-menu-right');
+       setTimeout(
+			function() {
+			$('#gallery-images-holder .card').waypoint(function() {
+				$('#gallery-images-holder .card').addClass('fadeInUp');
+			}, { offset: '500%' });
+		}, 1200);
     }
 
     $('#home-menu').hover(
         function(e){
-          //alert("test1");
           $('#my-work .side-black .side-border').css({'width':'20px','transition': '.5s'});
-        }, // over
+        },
         function(e){
-          //alert("test2");
           $('#my-work .side-black .side-border').css({'width':'0px','transition': '.5s'});
-        }  // out
+        }
     );
+
+    // hide our element on page load
+	  
+
+
+	 //   $('#animateheader').css('opacity', 0);
+
+  // $('#animateheader').waypoint(function() {
+  //     $('#animateheader').addClass('fadeInUp');
+  // }, { offset: '70%' });
 
     // $("#gallery-images-holder .card").slice(0, 6).show();
     // $("#loadMore").on('click', function (e) {
