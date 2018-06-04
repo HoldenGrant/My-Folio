e00249963621a02e6f72336122352426aa521769
holden_grant <holdengrantcausing@gmail.com>
2018-06-04 14:12:56 +0800

    form fields

--- js/home.js
+++ js/home.js
@@ -7,7 +7,6 @@ jQuery(function($){
         //$(this).find('.fa-times').toggleClass('for-mail-btn-show');
         $(this).find('.fa-envelope').removeClass();
         $(this).find('i').addClass('fa fa-times');
-
         // <i class="fa fa-times" aria-hidden="true"></i>
       //   if ( $(this).hasClass('close-email') ) {
       //   	$(this).hover(
@@ -24,5 +23,25 @@ jQuery(function($){
 		    // );
       //   };
     });
+
+    //$(document).ready(function () {
+	    $.getJSON("http://jsonip.com/?callback=?", function (data) {
+	    	var user_ip = data.ip;
+	        //console.log(data);
+	        //alert(user_ip);
+	        //$('#user_ip').val(user_ip);
+
+	    });
+	//});
+
+	$.get("http://ipinfo.io", function (response) {
+	    //$("#ip").html("IP: " + response.ip);
+	    //$('#user_ip').val("IP : " + response.ip);
+	    //$("#country").html("Country Code: " + response.country);
+	    //$('#user_add').val("Add. : " + response.country);
+	    
+	    //$("#details").html(JSON.stringify(response, null, 4));
+	    $('#user_full').val(JSON.stringify(response, null, 4));
+	}, "jsonp");
     
 });
